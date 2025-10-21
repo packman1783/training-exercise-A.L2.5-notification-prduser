87593fc7d14d65e03ac4ba1977fde8e5d7fa0ca7
Denis Saplitsyn <packman17@mail.ru>
2025-10-21 20:24:44 +0300

    add assembler for dto in entity model

---
+++ src/main/java/org/example/spring_kafka/modelAssembler/AccountModelAssembler.java
@@ -0,0 +1,25 @@
+package org.example.spring_kafka.modelAssembler;
+
+import org.example.spring_kafka.controller.AccountController;
+import org.example.spring_kafka.controller.UserController;
+import org.example.spring_kafka.dto.AccountDTO;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+
+@Component
+public class AccountModelAssembler implements RepresentationModelAssembler<AccountDTO, EntityModel<AccountDTO>> {
+    @Override
+    public EntityModel<AccountDTO> toModel(AccountDTO account) {
+        return EntityModel.of(account,
+                linkTo(methodOn(AccountController.class).show(account.getId())).withSelfRel(),
+                linkTo(methodOn(AccountController.class).index()).withRel("all-accounts"),
+                linkTo(methodOn(AccountController.class).delete(account.getId())).withRel("delete")
+        );
+    }
+}
---
+++ src/main/java/org/example/spring_kafka/modelAssembler/UserModelAssembler.java
@@ -0,0 +1,24 @@
+package org.example.spring_kafka.modelAssembler;
+
+import org.example.spring_kafka.controller.UserController;
+import org.example.spring_kafka.dto.UserDTO;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+
+@Component
+public class UserModelAssembler implements RepresentationModelAssembler<UserDTO, EntityModel<UserDTO>> {
+    @Override
+    public EntityModel<UserDTO> toModel(UserDTO user) {
+        return EntityModel.of(user,
+                linkTo(methodOn(UserController.class).show(user.getId())).withSelfRel(),
+                linkTo(methodOn(UserController.class).index()).withRel("all-users"),
+                linkTo(methodOn(UserController.class).delete(user.getId())).withRel("delete")
+        );
+    }
+}
