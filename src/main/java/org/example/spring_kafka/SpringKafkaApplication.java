12f9a2331d542837450dc9b5af4cd86f7a7cd860
Denis Saplitsyn <packman17@mail.ru>
2025-10-24 15:07:27 +0300

    remove unused import

--- src/main/java/org/example/spring_kafka/SpringKafkaApplication.java
+++ src/main/java/org/example/spring_kafka/SpringKafkaApplication.java
@@ -1,10 +1,7 @@
 package org.example.spring_kafka;
 
-import java.util.Locale;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringKafkaApplication {
