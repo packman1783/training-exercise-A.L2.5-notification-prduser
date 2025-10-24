e17e677ef49ca35b5781992acb1d85998e522d93
Denis Saplitsyn <packman17@mail.ru>
2025-10-24 15:06:30 +0300

    remove faker

--- src/main/java/org/example/spring_kafka/SpringKafkaApplication.java
+++ src/main/java/org/example/spring_kafka/SpringKafkaApplication.java
@@ -2,8 +2,6 @@ package org.example.spring_kafka;
 
 import java.util.Locale;
 
-import net.datafaker.Faker;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -14,9 +12,4 @@ public class SpringKafkaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringKafkaApplication.class, args);
 	}
-
-    @Bean
-    public Faker getFaker() {
-        return new Faker(new Locale("en", "US"));
-    }
 }
